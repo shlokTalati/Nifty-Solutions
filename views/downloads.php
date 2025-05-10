e789d88c467b618b094305ef9de1441c28d68352
Shlok Talati <talati.shlok3@gmail.com>
2025-05-10 17:30:47 +0530

    Added pdfs as well as proper download page

--- views/downloads.php
+++ views/downloads.php
@@ -1,45 +1,45 @@
 <!-- Hero Section -->
 <!-- start page title -->
 <section class="pt-0 cover-background ipad-top-space-margin sm-pb-0" style="background-image:url('/public/images/dlp/dlp-inner.jpg');">
-    <div class="shape-image-animation bottom-0 p-0 w-100 d-none d-md-block">
-        <svg xmlns="http://www.w3.org/2000/svg" widht="3000" height="400" viewBox="0 180 2500 200" fill="#ffffff">
-            <path class="st1" d="M 0 250 C 1200 400 1200 50 3000 250 L 3000 550 L 0 550 L 0 250">
-                <animate
-                attributeName="d"
-                dur="5s"
-                values="M 0 250 C 1200 400 1200 50 3000 250 L 3000 550 L 0 550 L 0 250;
+  <div class="shape-image-animation bottom-0 p-0 w-100 d-none d-md-block">
+    <svg xmlns="http://www.w3.org/2000/svg" widht="3000" height="400" viewBox="0 180 2500 200" fill="#ffffff">
+      <path class="st1" d="M 0 250 C 1200 400 1200 50 3000 250 L 3000 550 L 0 550 L 0 250">
+        <animate
+          attributeName="d"
+          dur="5s"
+          values="M 0 250 C 1200 400 1200 50 3000 250 L 3000 550 L 0 550 L 0 250;
                 M 0 250 C 400 50 400 400 3000 250 L 3000 550 L 0 550 L 0 250;
                 M 0 250 C 1200 400 1200 50 3000 250 L 3000 550 L 0 550 L 0 250"
-                repeatCount="indefinite" />
-            </path>
-        </svg>
-    </div>
-    <div class="container">
-        <div class="row align-items-center justify-content-center h-500px sm-h-300px">
-            <div class="col-12 col-lg-8 col-md-10 position-relative text-center page-title-extra-large d-flex flex-wrap flex-column align-items-center justify-content-center" data-anime='{ "el": "childs", "translateY": [30, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
-                <div class="row justify-content-center mb-4">
-                    <div class="col-lg-10 text-center">
-                        <h1 class="fw-bold mb-3 text-white">Downloads & Information</h1>
-                        <p class="mb-0">Nifty Solutions Literature: Brochure, Price List, Feature Sheet, Presentations, Updates of News and Events, Release Notes and more…</p>
-                    </div>
-                </div>
-            </div>
+          repeatCount="indefinite" />
+      </path>
+    </svg>
+  </div>
+  <div class="container">
+    <div class="row align-items-center justify-content-center h-500px sm-h-300px">
+      <div class="col-12 col-lg-8 col-md-10 position-relative text-center page-title-extra-large d-flex flex-wrap flex-column align-items-center justify-content-center" data-anime='{ "el": "childs", "translateY": [30, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
+        <div class="row justify-content-center mb-4">
+          <div class="col-lg-10 text-center">
+            <h1 class="fw-bold mb-3 text-white">Downloads & Information</h1>
+            <p class="mb-0">Nifty Solutions Literature: Brochure, Price List, Feature Sheet, Presentations, Updates of News and Events, Release Notes and more…</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </section>
 
 <hr>
 
 <div class="container">
-  <div class="accordion" id="mainDownloadsAccordion">
+  <div class="accordion" id="mainProductDownloadsAccordion">
     <!-- Parent Accordion Item -->
     <div class="accordion-item">
-      <h2 class="accordion-header" id="mainDownloadsHeading">
-        <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMainDownloads" aria-expanded="false" aria-controls="collapseMainDownloads">
+      <h2 class="accordion-header" id="mainProductDownloadsHeading">
+        <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMainProductDownloads" aria-expanded="false" aria-controls="collapseMainProductDownloads">
           Product Downloads
         </button>
       </h2>
-      <div id="collapseMainDownloads" class="accordion-collapse collapse" aria-labelledby="mainDownloadsHeading" data-bs-parent="#mainDownloadsAccordion">
+      <div id="collapseMainProductDownloads" class="accordion-collapse collapse" aria-labelledby="mainProductDownloadsHeading" data-bs-parent="#mainProductDownloadsAccordion">
         <div class="accordion-body">
           <!-- Start of nested accordion -->
           <div class="accordion accordion-flush" id="downloadsAccordion">
@@ -226,64 +226,54 @@
     </div>
 
     <div class="accordion-item">
-              <h2 class="accordion-header" id="headingEight">
-                <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight">
-                  Product Brochures
-                  
-                </button>
-              </h2>
-              <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#downloadsAccordion">
-                <div class="accordion-body">
+      <h2 class="accordion-header" id="headingEight">
+        <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#mainProductBrochuresCollapse">
+          Product Brochures
 
-                  <div class="mb-3">
-                    <h5 class="fw-bold">Windows 32-bit</h5>
-                    <ul class="list-unstyled">
-                      <li><a href="http://download.qlc.co.in/msi/WindowsInstaller-KB893803-v2-x86.exe">&raquo; Installer 3.1 x86</a></li>
-                    </ul>
-                  </div>
+        </button>
+      </h2>
+      <div id="mainProductBrochuresCollapse" class="accordion-collapse collapse" data-bs-parent="#downloadsAccordion">
+        <div class="accordion-body">
 
-                  <div class="mb-3">
-                    <h5 class="fw-bold">Windows 64-bit</h5>
-                    <ul class="list-unstyled">
-                      <li><a href="http://download.qlc.co.in/msi/WindowsInstaller-KB893803-v2-x64.exe">&raquo; Installer 3.1 x64</a></li>
-                    </ul>
-                  </div>
+          <div class="mb-3">
+            <h5 class="fw-bold"></h5>
+            <ul class="list-unstyled">
+              <li><a href="/public/pdf/Nova_Backup_Brochure-Product-Portfolio.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> Novastor - Data Backup Software : Feature Sheet</a></li>
+              <li><a href="/public/pdf/Iperius_data_backupsheet.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> Iperius - Data Backup Software : Feature Sheet</a></li>
+              <li><a href="/public/pdf/IW_Product_Brochure.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> IceWarp - Ultimate All-in-One E-mail and Collaboration Platform : Feature Sheet</a></li>
+              <li><a href="/public/pdf/mailvault-brochure.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> MailVault - Centralised Email Backup Software : Feature Sheet</a></li>
+              <li><a href="/public/pdf/mailstore-server-product-overview-en.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> MailStore - Centralised Email Backup Software : Feature Sheet</a></li>
+              <li><a href="/public/pdf/eScan-Total-Security-Suite-for-Business.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> eScan - Advanced Protection against Virus and Ransomware Threats : Feature Sheet</a></li>
+              <li><a href="/public/pdf/GZ_Business_Security_Datasheet.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> BitDefender - Cloud-based Advanced Protection against Virus and Ransomware Threats : Feature Sheet</a></li>
+              <li><a href="/public/pdf/PostMaster_Enterprise_Email Server_Brochure.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i> PostMaster : Feature Sheet</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
 
-                </div>
-              </div>
-            </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingEight">
+        <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#mainCompanyBrochureCollapse">
+          Company Brochure
+        </button>
+      </h2>
+      <div id="mainCompanyBrochureCollapse" class="accordion-collapse collapse" data-bs-parent="#downloadsAccordion">
+        <div class="accordion-body">
 
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingEight">
-                <button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight">
-                  Company Brochure
-                </button>
-              </h2>
-              <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#downloadsAccordion">
-                <div class="accordion-body">
+          <div class="mb-3">
+            <ul class="list-unstyled">
+              <li><a href="/public/pdf/Nifty_Solutions_profile.pdf" target="_blank">&raquo; <i class="bi bi-file-pdf-fill"></i>Nifty Solutions - Company Profile</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
 
-                  <div class="mb-3">
-                    <h5 class="fw-bold">Windows 32-bit</h5>
-                    <ul class="list-unstyled">
-                      <li><a href="http://download.qlc.co.in/msi/WindowsInstaller-KB893803-v2-x86.exe">&raquo; Installer 3.1 x86</a></li>
-                    </ul>
-                  </div>
-
-                  <div class="mb-3">
-                    <h5 class="fw-bold">Windows 64-bit</h5>
-                    <ul class="list-unstyled">
-                      <li><a href="http://download.qlc.co.in/msi/WindowsInstaller-KB893803-v2-x64.exe">&raquo; Installer 3.1 x64</a></li>
-                    </ul>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-    
   </div>
 
 
-  
+
 </div>
 
 <hr>
