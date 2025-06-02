4ea09ff1caf4218255c5d5592a8e2647abeb7b72
Shlok Talati <talati.shlok3@gmail.com>
2025-06-02 18:13:49 +0530

    Removed HOW IT WORKS from all pages using d-none

--- public/images/wed-hosting/backup-and-recovery/disaster_recovery.php
+++ public/images/wed-hosting/backup-and-recovery/disaster_recovery.php
@@ -170,7 +170,7 @@
                                     </div>
                                 </div>
                                 <div class="absolute-middle-center text-white-space-nowrap z-index-9">
-                                    <a href="https://www.youtube.com/watch?v=cfXHhfNy7tU" class="btn btn-extra-large btn-white left-icon btn-box-shadow fw-600 btn-rounded popup-youtube ls-minus-05px"><i class="fa-brands fa-youtube icon-small"></i>How it works</a>
+                                    <a href="javascript:void(0)" rel="nofollow" class="d-none btn btn-extra-large btn-white left-icon btn-box-shadow fw-600 btn-rounded popup-youtube ls-minus-05px"><i class="fa-brands fa-youtube icon-small"></i>How it works</a>
                                 </div>
                             </div>
                         </div>
--- views/endpoint-security/antivirus_and_content_security.php
+++ views/endpoint-security/antivirus_and_content_security.php
@@ -220,7 +220,7 @@
                                     </div>
                                 </div>
                                 <div class="absolute-middle-center text-white-space-nowrap z-index-9">
-                                    <a href="#" class="btn btn-extra-large btn-white left-icon btn-box-shadow fw-600 btn-rounded popup-youtube ls-minus-05px"><i class="fa-brands fa-youtube icon-small"></i>How it works</a>
+                                    <a href="#" class="d-none btn btn-extra-large btn-white left-icon btn-box-shadow fw-600 btn-rounded popup-youtube ls-minus-05px"><i class="fa-brands fa-youtube icon-small"></i>How it works</a>
                                 </div>
                             </div>
                         </div>
